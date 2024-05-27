32249394c8d393a9d6a6b33ab4146096751b160a
zhan <zhshy11@126.com>
2024-05-28 00:16:26 +0800

    add rize tvl

--- projects/rize/index.js
+++ projects/rize/index.js
@@ -3,17 +3,22 @@ const axios = require("axios");
 const BigNumber = require("bignumber.js");
 
 async function tvl(_, _1, _2, {api}) {
-    const result = await axios.get("https://referral-dev.m-safe-dev.link/tvl")
+    const result = await axios.get("https://referral.rizefi.io/tvl")
     const data = result.data
-    return {
-        [`ethereum:${addresses.null}`]: new BigNumber(data.coins.find(it => it.coinType === "ethereum").amount).times(1e18).toString(),
-        [`ethereum:${addresses.ethereum.USDT}`]: new BigNumber(data.coins.find(it => it.coinType === "usdt").amount).times(1e6).toString(),
-        [`ethereum:${addresses.ethereum.USDC}`]: new BigNumber(data.coins.find(it => it.coinType === "usdc").amount).times(1e6).toString(),
-    }
+    const tvl ={};
+    data.coins.forEach(coin =>{
+        if (coin.coinType === "ethereum"){
+            tvl[`ethereum:${addresses.null}`] = new BigNumber(coin.amount).times(1e18).toString();
+        }else if(addresses.ethereum.hasOwnProperty(coin.coinType.toUpperCase())){
+            console.log(`ethereum:${addresses.ethereum[coin.coinType.toUpperCase()]}`)
+            tvl[`ethereum:${addresses.ethereum[coin.coinType.toUpperCase()]}`] = new BigNumber(coin.amount).times(1e6).toString();
+        }
+    })
+    return tvl;
 }
 
 module.exports = {
-    start: 1713892926,
+    start: 19944102,
     ethereum: {
         tvl
     },
